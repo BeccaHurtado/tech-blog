88cf45b2002aded34e1b4bdee4082c4ada0e97e7
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-29 16:51:10 -0700

    created user model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+// model and datatypes imported from sequelize program
+const { Model, DataTypes } = require('sequelize');
+
+// protect user password
+
+// import connection
+const sequelize = require('../config/connection');
+
+// create User model
+class User extends Model {
+    // set up password authentication
+}
+
+// table columns & configuration
+User.unit(
+    {
+        // id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        //username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // length of password
+                len: [6]
+            }
+        }
+    },
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+    }   
+);
+
+model.exports = User
