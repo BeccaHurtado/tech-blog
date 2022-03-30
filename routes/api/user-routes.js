ed35d8905bddc992b36abc771e7cde95403d3641
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-29 17:00:57 -0700

    created user routes file

---
+++ routes/api/user-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET all users
+router.get('/', (req, res) => {
+
+})
+// GET one user
+router.get('/', (req, res) => {
+
+})
+// POST create new user
+router.get('/', (req, res) => {
+
+})
+// POST for user login
+router.get('/', (req, res) => {
+
+})
+// PUT update a user
+router.get('/', (req, res) => {
+
+})
+// DELETE delete a user
+router.get('/', (req, res) => {
+
+})
+
