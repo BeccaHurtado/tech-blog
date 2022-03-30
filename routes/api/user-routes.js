778abb5b574455e996af17f4b6589e3ccea8e6ac
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-29 17:03:47 -0700

    added find all users route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,8 +3,16 @@ const { User } = require('../../models');
 
 // GET all users
 router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-})
 // GET one user
 router.get('/', (req, res) => {
 
